df6131e538ab0726678de8c37b7b51c43c8246ec
James Graca-Jones <jamesgracajones@gmail.com>
2024-08-10 18:56:18 +0100

    Make lists in FeaturedUnfeatured into flex containers

--- components/FeaturedUnfeatured/FeaturedUnfeatured.tsx
+++ components/FeaturedUnfeatured/FeaturedUnfeatured.tsx
@@ -1,18 +1,19 @@
 import ComponentList from "@/components/ComponentList/ComponentList";
 import { FeaturedUnfeaturedProps } from "@/types";
+import styles from "./FeaturedUnfeatured.module.css";
 
 export default function FeaturedUnfeatured<T extends object>({ featuredProps, unfeaturedProps, ListedComponent }: FeaturedUnfeaturedProps<T>) {
     return (
         <>
           {featuredProps.length > 0 && <section>
               <h2>Featured...</h2>
-              <ol>
+              <ol className={styles.list}>
                 <ComponentList dataList={featuredProps} ListedComponent={ListedComponent}/>
               </ol>
           </section>}
           {unfeaturedProps.length > 0 && <section>
               <h2>{unfeaturedProps.length ? 'More' : 'Featured'}...</h2>
-              <ol>
+              <ol className={styles.list}>
                 <ComponentList dataList={unfeaturedProps} ListedComponent={ListedComponent}/>
               </ol>
           </section>}
