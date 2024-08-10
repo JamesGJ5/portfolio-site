3bba39d6c54636a41a6f76fc994d3e77f4e49fb5
James Graca-Jones <jamesgracajones@gmail.com>
2024-08-10 19:11:24 +0100

    Make certifications list a flex container

--- app/certifications/page.tsx
+++ app/certifications/page.tsx
@@ -1,12 +1,13 @@
 import Certification from "@/components/Certification/Certification";
 import ComponentList from "@/components/ComponentList/ComponentList";
 import { certifications } from "@/data/certifications";
+import styles from "./page.module.css";
 
 export default function CertificationsPage() {
     return (
         <>
             <h1>My Certifications...</h1>
-            <ol>
+            <ol className={styles.certificationList}>
                 <ComponentList dataList={certifications.map(certification => ({certification}))} ListedComponent={Certification}/>
             </ol>
         </>
